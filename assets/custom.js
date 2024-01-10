dcc284ffdf27b897cf8f0c722960b304c4761a33
Yeshhub <156052357+Yeshhub@users.noreply.github.com>
2024-01-10 12:53:51 +0530

    Swatches v3

--- assets/custom.js
+++ assets/custom.js
@@ -1,11 +1,17 @@
-// Assuming there is only one element with class 'product-form__input--dropdown'
+// Targeting element with class 'product-form__input--dropdown'
 var dropdown = document.querySelector('.product-form__input--dropdown');
 
-// Assuming there is only one element with class 'swatches'
+// Targeting element with class 'swatches'
 var swatches = dropdown.querySelector('.swatches');
 
-// Assuming there is only one element with class 'select__select' within the next sibling of 'swatches'
+// Targeting element with class 'select__select' within the next sibling of 'swatches'
 var select = swatches.nextElementSibling.querySelector('.select__select');
+
+// Targeting element with class 'product-form__input--dropdown' for size
+var sizeDropdown = document.querySelector('.product-form__input--dropdown.size');
+
+// Targeting element with class 'select__select' within 'sizeDropdown'
+var sizeSelect = sizeDropdown.querySelector('.select__select');
 
 // Get all elements with class 'swatch' within 'swatches'
 var swatchElements = swatches.querySelectorAll('.swatch');
@@ -34,6 +40,19 @@ swatchElements.forEach(function(swatch) {
     var selectedOption = select.querySelector('option[value="' + selectedColor + '"]');
     if (selectedOption) {
       selectedOption.setAttribute('selected', 'selected');
+    }
+
+    // Get the selected size value from the size dropdown
+    var selectedSize = sizeSelect.value;
+
+    // Clear and set the selected size for the size dropdown
+    sizeSelect.value = '';
+    sizeSelect.value = selectedSize;
+
+    // Trigger click event on the option with the selected size value
+    var sizeOption = sizeSelect.querySelector('option[value="' + selectedSize + '"]');
+    if (sizeOption) {
+      sizeOption.click();
     }
 
     // Add 'selected' class to the clicked 'swatch' element
